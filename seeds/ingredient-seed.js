52e83e151e65fd0d74b6792de4a5d95b9341dec5
Heather Stevens <heatherveva@gmail.com>
2022-03-21 19:53:16 -0500

    added two seeds files and images

---
+++ seeds/ingredient-seed.js
@@ -0,0 +1,158 @@
+const { Ingredient } = require('../models');
+
+const ingredientData = [
+  {
+    name: 'Shrimp',
+  },
+  {
+    name: 'Salmon',
+  },
+  {
+    name: 'Ground Beef',
+  },
+  {
+    name: 'Chicken',
+  },
+  {
+    name: 'Egg',
+  },
+  {
+    name: 'Salmon',
+  },
+  {
+    name: 'Pepperoni',
+  },
+  {
+    name: 'Bacon',
+  },
+  {
+    name: 'Turkey',
+  },
+  {
+    name: 'Sausage',
+  },
+  {
+    name: 'Rice',
+  },
+  {
+    name: 'Bacon',
+  },
+  {
+    name: 'Pasta',
+  },
+  {
+    name: 'Bread',
+  },
+  {
+    name: 'Tortillas',
+  },
+  {
+    name: 'Ravioli',
+  },
+  {
+    name: 'Soba Noodles',
+  },
+  {
+    name: 'Pizza Dough',
+  },
+  {
+    name: 'Chipolte Peppers',
+  },
+  {
+    name: 'Scallions',
+  },
+  {
+    name: 'Carrots',
+  },
+  {
+    name: 'Bean Sprouts',
+  },
+  {
+    name: 'Onion',
+  },
+  {
+    name: 'Potatoes',
+  },
+  {
+    name: 'Kale',
+  },
+  {
+    name: 'Tomatos',
+  },
+  {
+    name: 'Cucumber',
+  },
+  {
+    name: 'Chicken Broth',
+  },
+  {
+    name: 'Cucumber',
+  },
+  {
+    name: 'Lemon',
+  },
+  {
+    name: 'Lime',
+  },
+  {
+    name: 'White Wine',
+  },
+  {
+    name: 'Mustard',
+  },
+  {
+    name: 'Almonds',
+  },
+  {
+    name: 'Walnuts',
+  },
+  {
+    name: 'Brown Sugar',
+  },
+  {
+    name: 'Miso Paste',
+  },
+  {
+    name: 'Kidney Beans',
+  },
+  {
+    name: 'Stout Beer',
+  },
+  {
+    name: 'Mustard',
+  },
+  {
+    name: 'Heavy Cream',
+  },
+  {
+    name: 'Parmesean Cheese',
+  },
+  {
+    name: 'Cheddar Cheese',
+  },
+  {
+    name: 'Cream Cheese',
+  },
+  {
+    name: 'Blue Cheese',
+  },
+  {
+    name: 'Mozerella Cheese',
+  },
+  {
+    name: 'Mushrooms',
+  },
+  {
+    name: 'Bread Crumbs',
+  },
+  {
+    name: 'Ranch Mix',
+  },
+  {
+    name: 'Sour Cream',
+  },
+];
+
+const seedIngredients = () => Ingredient.bulkCreate(ingredientData);
+
+module.exports = seedIngredients;
---
+++ seeds/recipe-seed.js
@@ -0,0 +1,123 @@
+const { Recipe } = require('../models');
+
+const recipeData = [
+  {
+    name: 'Shrimp Scampi',
+    description:
+      'Quick and tasty shrimp and pasta, finished with lemon and red pepper flakes.',
+    image_url: '',
+  },
+  {
+    name: 'Buffalo Chicken Enchiladas',
+    description:
+      'Stuff corn tortillas with a cheesy Buffalo filling and bake until melted for an easy weeknight meal',
+    image_url: '',
+  },
+  {
+    name: 'Glazed Salmon and Spiced Carrots',
+    description:
+      'Honey-mustard glazed salmon and serve it with carrots tossed in a spiced lime and almond dressing.',
+    image_url: '',
+  },
+  {
+    name: 'Ravioli with Sage and Walnut Butter',
+    description:
+      'Store bought ravioli gets an upgrade with this buttery sage and walnut butter.',
+    image_url: '',
+  },
+  {
+    name: 'Turkey Pad Thai',
+    description:
+      'Loaded with fresh veggies, this take on pad thai is hearty and healthy.',
+    image_url: '',
+  },
+  {
+    name: 'Garlic Butter Baked Salmon',
+    description:
+      'Quick and easy, this garlic butter glazed baked salmon is a tasty, weeknight meal.',
+    image_url: '',
+  },
+  {
+    name: 'Chipotle Chicken Skewers',
+    description: 'The grilled chicken skewers pack a bit of spice.',
+    image_url: '',
+  },
+  {
+    name: 'Hamburger',
+    description: 'An American classic with an upgrade.',
+    image_url: '',
+  },
+  {
+    name: 'Potato Wedges',
+    description:
+      'A one pan meal. Perfectly seasoned and baked until crispy, easy and tasty.',
+    image_url: '',
+  },
+  {
+    name: 'Zuppa Toscana',
+    description:
+      'Creamy and full of flavor, this is the perfect recipe for this classic Italian soup.',
+    image_url: '',
+  },
+  {
+    name: 'Pasta Salad',
+    description:
+      'Fresh and easy pasta salad packed with crisp vegetables, fresh mozzarella, and tossed with a simple homemade dressing.',
+    image_url: '',
+  },
+  {
+    name: 'Cool Summer Soba Noodle Salad',
+    description:
+      'A refreshing summer salad with veggies and an Asian inspired dressing.',
+    image_url: '',
+  },
+  {
+    name: 'Parmesan Bread Twist',
+    description:
+      'Made with store bought dough, this quick recipe will ease your craving for Italian takeout.',
+    image_url: '',
+  },
+  {
+    name: 'Chipotle Stout Chili',
+    description:
+      'Filled with slow cooked meat and veggies, this chili is an unbeatable weeknight meal.',
+    image_url: '',
+  },
+  {
+    name: 'Chicken Marsala',
+    description: 'Creamy, slow simmered chicken in a rich sauce.',
+    image_url: '',
+  },
+  {
+    name: 'Chili Glazed Salmon',
+    description: 'Baked salmon with a sweet and spicy glaze.',
+    image_url: '',
+  },
+  {
+    name: 'Baked Ranch Chicken',
+    description:
+      'Baked chicken coated in ranch seasoning and bread crumbs for a flavorful, crispy dinner.',
+    image_url: '',
+  },
+  {
+    name: 'Baked Shrimp',
+    description:
+      'A perfect, one pan meal. These shrimp are perfectly seasoned and ready in just a few minutes.',
+    image_url: '',
+  },
+  {
+    name: 'Cheeseburger Stuffed Bell Pepper',
+    description: 'A healthy, veggie forward take on a classic weeknight meal.',
+    image_url: '',
+  },
+  {
+    name: 'Baked Mac & Cheese',
+    description:
+      'A grown up version of a childhood classic, this cheesy pasta is finished in the oven for a crispy upgrade.',
+    image_url: '',
+  },
+];
+
+const seedRecipes = () => Recipe.bulkCreate(recipeData);
+
+module.exports = seedRecipes;
