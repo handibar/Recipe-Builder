27b0d703a656e71981bd20476984f9dc22349bf5
Jason B <jasonsburke90@gmail.com>
2022-03-24 17:11:07 -0500

    incorporated deletes

---
+++ public/js/profile.js
@@ -0,0 +1,19 @@
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/recipes/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete favorite');
+    }
+  }
+};
+
+document
+  .querySelector('.favorite-list')
+  .addEventListener('click', delButtonHandler);
