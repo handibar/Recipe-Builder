e85708e10720c83afbbbfc418eb2b1f97979c7fb
Jason B <jasonsburke90@gmail.com>
2022-03-24 17:50:23 -0500

    fixed seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,17 @@
 const sequelize = require('../config/connection');
+const { Ingredient, IngredientRecipe, Recipe } = require('../models');
 
+const ingredientData = require('./ingredientData.json');
+const recipeData = require('./recipeData.json');
+const ingredientRecipeData = require('./ingredientrecipeData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: false });
-
-
+  const seedIngredients = await Ingredient.bulkCreate(ingredientData);
+  const seedRecipes = await Recipe.bulkCreate(recipeData);
+  const seedIngredientRecipes = await IngredientRecipe.bulkCreate(
+    ingredientRecipeData
+  );
   process.exit(0);
 };
 
