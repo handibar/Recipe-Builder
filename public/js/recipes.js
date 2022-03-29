d1f9214ff80eeb356fa53a75ba06ee105f9ac31e
Maya Tillman <mayatillman17@gmail.com>
2022-03-29 18:19:30 -0500

    design

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -1,12 +1,20 @@
 //create function if favorite button is clicked
+const btn = document.getElementById('fav');
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'white';
+  btn.style.color = 'black';
+  btn.innerHTML = 'Added!';
+});
 
 const newFavoriteHandler = async (event) => {
   event.preventDefault();
+  console.log('clicked');
 
   // get recipeid from button when clicked
 
   const recipe_id = event.target.value;
-
+  console.log(recipe_id);
   if (recipe_id) {
     const response = await fetch(`/api/recipes/favorites`, {
       method: 'POST',
@@ -17,6 +25,7 @@ const newFavoriteHandler = async (event) => {
     });
 
     if (response.ok) {
+      console.log('favorite added');
     } else {
       alert(
         'Failed to create favorite - you cannot favorite the same recipe twice!'
